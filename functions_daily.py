6b6455261813d7703349f87075f25e09ff8372e8
Giulio Yongco <yongcogiulio125@gmail.com>
2024-07-19 18:38:49 +0800

    correction of sleep

--- functions_daily.py
+++ functions_daily.py
@@ -14,9 +14,9 @@ from datetime import datetime,time, timedelta
 
 # GLOBAL VARIABLES =======================================================
 
-MIN_SECONDS 	= 60
-HOUR_SECONDS 	= MIN_SECONDS * 60
-DAY_SECONDS 	= HOUR_SECONDS * 24
+MINUTE_SECONDS:int 	= 60
+HOUR_SECONDS:int 	= MINUTE_SECONDS * 60
+DAY_SECONDS:int 	= HOUR_SECONDS * 24
 
 # ========================================================================
 # FUNCTIONS MISC
@@ -36,24 +36,28 @@ def nearest_divisible_int_more(num:int = 420, divisible_by = 69) -> int:
 def raw_seconds(time:time = datetime.now().time()):
 	return (time.hour * 3600) + (time.minute * 60) + time.second
 
-async def bot_time_log(channel:discord.TextChannel, interval_seconds:int = 5):
+def interval_synchronize(interval_seconds:int = MINUTE_SECONDS) -> int:
+	return interval_seconds - (raw_seconds()%interval_seconds)
+
+async def bot_time_log(channel:discord.TextChannel, interval_seconds:int = MINUTE_SECONDS):
 	print("function : bot_time_log started")
 	
-	cycles:int = 0
+	cycle_num:int = 0
 
 	# sleeps so that the log update will match the intervals
 	# ex: if interval is 5 mins, log will happen in a minute divisible by 5
-	await asyncio.sleep(nearest_divisible_int_more(raw_seconds(), interval_seconds) - raw_seconds())
+	await asyncio.sleep(interval_synchronize(interval_seconds = interval_seconds))
 
 	while True:
-		up_time: str = f"{'up time':20}:{cycles}"
+		up_time: str = f"{'up time':20}:{cycle_num}"
 		currrent_time : str = f"{'cycle number':20}:{datetime.now().strftime('%Y|%d|%M - %H:%M:%S')}"
 		await channel.send(f"```I am still alive\n{up_time}\n{currrent_time}```")
 
-
-
-		await asyncio.sleep(interval_seconds)
-		cycles += 1
+		if cycle_num%10 == 0:
+			await asyncio.sleep(interval_synchronize(interval_seconds = interval_seconds))
+		else:
+			await asyncio.sleep(interval_seconds)
+		cycle_num += 1
 
 # ========================================================================
 # TEST 
@@ -66,6 +70,11 @@ if __name__ == '__main__':
 	print(nearest_divisible_int_more(num=420, divisible_by=69))
 	print(nearest_divisible_int_more(num=43, divisible_by=5))
 
+	print(datetime.now().time())
+	seconds = raw_seconds()
 	print(raw_seconds())
-	
+	print(f"raw = {int(seconds/HOUR_SECONDS)}:{int((seconds%HOUR_SECONDS)/MINUTE_SECONDS)}:{seconds%MINUTE_SECONDS}")
+	interval_seconds:int = 300
+	print(interval_seconds - (raw_seconds()%interval_seconds))
+
 	section("END")
