9ce7bf07e53f070fce157ace112f65521948a239
Giulio Yongco <130813239+GM-Yongco@users.noreply.github.com>
2024-11-22 21:20:42 +0800

    deleted unnecessary functionality

--- commands_channels.py
+++ commands_channels.py
@@ -6,67 +6,23 @@
 
 import discord
 import discord.ext.commands
-from discord import app_commands
-
-import time
 
 # ========================================================================
 # FUNCTIONS 
 # ========================================================================
 
-def define_commands_channels(bot:discord.ext.commands.bot.Bot) -> None:
+def define_commands_channels(bot:discord.ext.commands.bot.Bot, authorized_user_id:str) -> None:
 
-		# ================================================================
+	@bot.tree.command(name = "channel_clone_delete", description = "clones channels the command is done in")
+	async def channel_clone_delete(interaction: discord.Interaction):
 
-		@bot.tree.command(name = "get_this_channel", description = "gets the currents channels details")
-		async def get_this_channel(interaction: discord.Interaction):
-			print("command : get_this_channel ")
+		function_prefix:str = "command : channel_clone_delete"
 
-			message:str = ""
-
-			# interaction.channel_id can return has type int|None
-			if(interaction.channel_id != None):
-				message += f"\ntype\t: {type(interaction.channel)}"
-				message += f"\nname\t: '{str(interaction.channel.name)}'"
-				message += f"\nid\t: {str(interaction.channel_id)}"
-			else:
-				message += f"channel id of channel does not exist"
-			
-			await interaction.response.send_message(f"{message}")
-			
-		# ================================================================
-
-		@bot.tree.command(name = "get_channel", description = "gets channel details")
-		@app_commands.describe(channel = 'discord native : channel to be deleted')
-		async def get_channel(
-				interaction: discord.Interaction, 
-				channel:discord.TextChannel|discord.VoiceChannel|discord.Thread
-			):
-			print("command : get_channel ")
-
-			message:str = ""
-
-			# interaction.channel_id can return has type int|None
-			if(interaction.channel_id != None):
-				message += f"\ntype\t: {type(channel)}"
-				message += f"\nname\t: '{str(channel)}'"
-				message += f"\nid\t: {str(channel.id)}"
-			else:
-				message += f"channel id of channel does not exist"
-			
-			await interaction.response.send_message(f"{message}")
-
-		# ================================================================
-
-		@bot.tree.command(name = "channel_clone_delete", description = "clones channels the command is done in")
-		async def channel_clone_delete(interaction: discord.Interaction):
-			print("command : channel_clone_delete ")
-
-			delay_seconds:int = 5
-			await interaction.response.send_message(f"deletion in {delay_seconds}")
-
+		# checking credentials
+		if interaction.user.id == int(authorized_user_id):
 			await interaction.channel.clone()
-			for i in range(delay_seconds, 0, -1):
-				time.sleep(1)
-				await interaction.channel.send(i)
 			await interaction.channel.delete()
+			print(f"{function_prefix} authorized")
+		else:
+			await interaction.response.send_message("you dont have credentials for this bucko")
+			print(f"{function_prefix} not authorized")
