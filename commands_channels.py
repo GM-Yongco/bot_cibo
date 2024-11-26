cd870c5fd4d5976e6a524d4b03239492f6ed726d
Giulio Yongco <130813239+GM-Yongco@users.noreply.github.com>
2024-11-26 20:30:38 +0800

    weird naming going on

--- commands_channels.py
+++ commands_channels.py
@@ -1,7 +1,7 @@
 # Author			: G.M. Yongco #BeSomeoneWhoCanStandByShinomiya
 # Date				: ur my date uwu
 # Description		: A template class to abstract away discord stuff
-# Real Description	: all channel based commands
+# Real Description	: all date based commands
 # HEADERS ================================================================
 
 import discord
