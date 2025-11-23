be04f07981d96a35d6cd6373145709b5958aa335
Giulio Yongco <yongcogiulio125@gmail.com>
2025-11-23 23:48:39 +0800

    updated autostart

--- autostart/autostart_cibo_bot.sh
+++ autostart/autostart_cibo_bot.sh
@@ -3,7 +3,7 @@ set -e  # stop on first error
 LOG="$HOME/.config/autostart/autostart_cibo_bot.log"
 exec > >(tee -a "$LOG") 2>&1
 
-tmux new -d -s cibo_bot
+tmux new -d -s cibo_bot | true
 tmux send-keys -t cibo_bot "cd ~"  C-m
 tmux send-keys -t cibo_bot "source venv_discord/bin/activate"  C-m
 tmux send-keys -t cibo_bot "cd ~/git/bot_cibo/bot" C-m
